a9af81756d6af7cee38370801655d3ff6027c20b
Carl Abel <caweb.cph@gmail.com>
2022-10-05 14:12:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [token, setToken] = useState(null);
+	const [secretMessage, setSecretMessage] = useState("");
+
+	useEffect(
+		function () {
+			fetch("/.netlify/functions/super-secret", {
+				headers: {
+					Authorization: "Bearer " + token,
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => setSecretMessage(data.message));
+		},
+		[token]
+	);
+	async function submitHandler(e) {
+		e.preventDefault();
+		try {
+			const response = await fetch("/.netlify/functions/auth", {
+				method: "POST",
+				body: JSON.stringify({
+					email: e.target.email.value,
+					password: e.target.password.value,
+				}),
+			});
+			if (response.status === 201) {
+				const data = await response.json();
+				setToken(data.token);
+			} else {
+			}
+			const data = await response.json();
+			setToken(data.token);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<div className="App">
+			<form onSubmit={submitHandler}>
+				<div>
+					<label>
+						Email:
+						<input
+							type="email"
+							name="email"
+						/>
+					</label>
+				</div>
+				<div>
+					<label>
+						Password:
+						<input
+							type="password"
+							name="password"
+						/>
+					</label>
+				</div>
+				<button type="submit"> Log in</button>
+			</form>
+			<p>{token ? "du er logget ind  " : "du er ikke logget ind "}</p>
+			<div>{secretMessage}</div>
+		</div>
+	);
 }
 
 export default App;
